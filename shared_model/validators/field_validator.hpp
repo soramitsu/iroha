fd9db1042f31f6ec832237ce0a7b3b63cbcdd015
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2019-05-10 13:30:44 +0300

    Command AddSmartContract with mocks
    Add field callee to AddSmartContract
    
    Add more fields to AddSmartContract
    
    Now AddSmartContract parameters are:
    caller, callee, code, input
    
    Add go sources to call Burrow VM
    
    Docker container should be runned as sudo.
    To set environment:
    1) source scripts/envForBurrowVM.sh
    2) ./scripts/buildVmCaller.sh
    
    To run test add_smart_contract:
    ./scripts/SC.sh
    
    Add instruction about add_smart_contract test
    
    Fix link issue of vmCall to tests
    
    Update vmCall to current Burrow code
    
    Fix test failures due to incomplete validators_fixture
    
    Signed-off-by: Ivan Tyulyandin <ivan.tyulyandin@gmail.com>

--- shared_model/validators/field_validator.hpp
+++ shared_model/validators/field_validator.hpp
@@ -52,6 +52,18 @@ namespace shared_model {
       void validateAssetId(ReasonsGroupType &reason,
                            const interface::types::AssetIdType &asset_id) const;
 
+      void validateCaller(ReasonsGroupType &reason,
+                          const interface::types::AccountIdType &caller) const;
+
+      void validateCallee(ReasonsGroupType &reason,
+                          const interface::types::AccountIdType &callee) const;
+
+      void validateCode(ReasonsGroupType &reason,
+                        const interface::types::SmartContractCodeType &code) const;
+
+      void validateInput(ReasonsGroupType &reason,
+                        const interface::types::SmartContractCodeType &input) const;
+
       void validatePeer(ReasonsGroupType &reason,
                         const interface::Peer &peer) const;
 
@@ -194,6 +206,7 @@ namespace shared_model {
       const static std::string asset_id_pattern_;
       const static std::string detail_key_pattern_;
       const static std::string role_id_pattern_;
+      const static std::string bytecode_pattern_;
 
       const static std::regex account_name_regex_;
       const static std::regex asset_name_regex_;
@@ -204,6 +217,7 @@ namespace shared_model {
       const static std::regex asset_id_regex_;
       const static std::regex detail_key_regex_;
       const static std::regex role_id_regex_;
+      const static std::regex bytecode_regex_;
 
       // gap for future transactions
       time_t future_gap_;
