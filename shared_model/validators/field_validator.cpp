8fc69d3d3656ad6a685b94ee657457f9fd8b297d
Andrei Lebedev <lebdron@gmail.com>
2019-08-20 14:25:11 +0300

    Fix errors related to command execution
    Signed-off-by: Andrei Lebedev <lebdron@gmail.com>

--- shared_model/validators/field_validator.cpp
+++ shared_model/validators/field_validator.cpp
@@ -28,7 +28,7 @@ namespace shared_model {
   namespace validation {
 
     const std::string FieldValidator::account_name_pattern_ =
-        R"#([a-z_0-9]{1,32})#";
+        R"#([a-zA-Z_0-9]{1,64})#";
     const std::string FieldValidator::asset_name_pattern_ =
         R"#([a-z_0-9]{1,32})#";
     const std::string FieldValidator::domain_pattern_ =
@@ -103,55 +103,53 @@ namespace shared_model {
     void FieldValidator::validateCaller(
         ReasonsGroupType &reason,
         const interface::types::AccountIdType &caller) const {
-        // TODO(IvanTyulyandin): add caller validator
-        // this is mock for tests to be passed
-        // consider accountId validation method to call
-        if (caller.empty()) {
-          auto message =
-              boost::format("Smart contract caller should be specified")
-                  .str();
-          reason.second.push_back(std::move(message));
-        }
+      // TODO(IvanTyulyandin): add caller validator
+      // this is mock for tests to be passed
+      // consider accountId validation method to call
+      if (caller.empty()) {
+        auto message =
+            boost::format("Smart contract caller should be specified").str();
+        reason.second.push_back(std::move(message));
+      }
     }
 
     void FieldValidator::validateCallee(
         ReasonsGroupType &reason,
         const interface::types::AccountIdType &callee) const {
-        // TODO(IvanTyulyandin): add callee validator
-        // this is mock for tests to be passed
-        // consider accountId validation method to call
-        if (callee.empty()) {
-          auto message =
-              boost::format("Smart contract callee should be specified")
-                  .str();
-          reason.second.push_back(std::move(message));
-        }
+      // TODO(IvanTyulyandin): add callee validator
+      // this is mock for tests to be passed
+      // consider accountId validation method to call
+      if (callee.empty()) {
+        auto message =
+            boost::format("Smart contract callee should be specified").str();
+        reason.second.push_back(std::move(message));
+      }
     }
 
     void FieldValidator::validateCode(
         ReasonsGroupType &reason,
         const interface::types::SmartContractCodeType &code) const {
-        // TODO(IvanTyulyandin): add code validator
-        // this is mock for tests to be passed
-        if (not std::regex_match(code, bytecode_regex_)) {
-          auto message =
-              boost::format("Smart contract code size must be a hex bytecode")
-                  .str();
-          reason.second.push_back(std::move(message));
-        }
+      // TODO(IvanTyulyandin): add code validator
+      // this is mock for tests to be passed
+      if (not std::regex_match(code, bytecode_regex_)) {
+        auto message =
+            boost::format("Smart contract code size must be a hex bytecode")
+                .str();
+        reason.second.push_back(std::move(message));
+      }
     }
 
     void FieldValidator::validateInput(
         ReasonsGroupType &reason,
         const interface::types::SmartContractCodeType &input) const {
-        // TODO(IvanTyulyandin): add code validator
-        // this is mock for tests to be passed
-        if (not std::regex_match(input, bytecode_regex_)) {
-          auto message =
-              boost::format("Smart contract input size must be a hex bytecode")
-                  .str();
-          reason.second.push_back(std::move(message));
-        }
+      // TODO(IvanTyulyandin): add code validator
+      // this is mock for tests to be passed
+      if (not std::regex_match(input, bytecode_regex_)) {
+        auto message =
+            boost::format("Smart contract input size must be a hex bytecode")
+                .str();
+        reason.second.push_back(std::move(message));
+      }
     }
 
     void FieldValidator::validatePeer(ReasonsGroupType &reason,
